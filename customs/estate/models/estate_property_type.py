e919945686aa2a523c5907233cf1ffbeb5023090
Munjata KEITA <munjata.keita@gmail.com>
2023-09-16 18:48:26 +0000

    Link offers to property Type

--- customs/estate/models/estate_property_type.py
+++ customs/estate/models/estate_property_type.py
@@ -1,4 +1,4 @@
-from odoo import fields, models
+from odoo import fields, models, api
 
 
 class PropertyType(models.Model):
@@ -8,8 +8,15 @@ class PropertyType(models.Model):
     name = fields.Char(string="Type", required=True)
     property_ids = fields.One2many("estate.property", "property_type_id")
     sequence = fields.Integer("Sequence", default=1, help="Used to sort Type. Lower is better")
+    offer_ids = fields.One2many("estate.property.offer", "property_type_id", string="Offers")
+    offer_count = fields.Integer(string="Offers Count", compute="_compute_offer_count")
 
     _order = "name"
     _sql_constraints = [
         ("name_unique", "UNIQUE(name)", "Tag name already exists!"),
     ]
+
+    @api.depends("offer_ids")
+    def _compute_offer_count(self):
+        for record in self:
+            record.offer_count = len(record.offer_ids)
--- customs/estate/models/property_offer.py
+++ customs/estate/models/property_offer.py
@@ -15,6 +15,7 @@ class PropertyOffer(models.Model):
     )
     partner_id = fields.Many2one("res.partner", string="Partner", required=True)
     property_id = fields.Many2one("estate.property", string="Property", required=True)
+    property_type_id = fields.Many2one(related="property_id.property_type_id", string="Property Type", store=True)
     validity = fields.Integer(string="Validity", default=7)
     date_deadline = fields.Date(string="Deadline", compute="_compute_deadline", inverse="_inverse_deadline")
 
